4f64cf299e788de849f07d40c0cb0278bcf90943
SamriddhI-ndia <jainsamriddhi10@gmail.com>
2021-07-13 17:05:13 +0530

    THA_17 & THA_20

--- calorie_reader/src/App.jsx
+++ calorie_reader/src/App.jsx
@@ -13,14 +13,20 @@ const [cards, setcards] = useState([
   {key:"5",text:"Paani Puri" ,calorie:"90"},
   {key:"6",text:"Fried Rice" ,calorie:"10"}
 ])
+const [text,setText]=useState("");
 const deletee=(key)=>{
   const newCards=cards.filter((card)=>card.key!==key)
   setcards(newCards);
+  if(newCards.length===0){
+    setText("No Item!")
+  }
 }
   return (
     <div className="App">
       <div className="head">
         <h1>Calorie Reader</h1>
+      
+      <div className="no">{text}</div>
       </div>
       <CardList cards={cards} handleDelete={deletee}/>
     </div>
